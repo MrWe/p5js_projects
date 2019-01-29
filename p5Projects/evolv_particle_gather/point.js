54caabf5c6487dd1e01b802209c135626bcf5a2f
mrwe <mr.wiker@gmail.com>
2019-01-29 15:58:20 +0100

    .

--- p5Projects/evolv_particle_gather/point.js
+++ p5Projects/evolv_particle_gather/point.js
@@ -4,14 +4,29 @@ class Point {
     this.pos = start_pos.copy();
     this.stop_pos = stop_pos.copy();
     this.direction = direction;
+    this.catched = false;
   }
 
   update(elements) {
+    if(this.catched){
+      return;
+    }
+    if(dist(this.pos.x, this.pos.y, this.stop_pos.x, this.stop_pos.y) < r/2){
+      this.catched = true;
+      this.parent.fitness += 1;
+      return;
+    }
+    for(let element of elements){
+      if(dist(this.pos.x, this.pos.y, element.pos.x, element.pos.y) < element.size/2){
+        this.direction.add(element.speed)
+      }
+    }
+  
     this.pos.add(this.direction);
+   
   }
 
   show() {
-    print(this.pos);
     push();
     fill(255);
     strokeWeight(0);
--- p5Projects/evolv_particle_gather/sketch.js
+++ p5Projects/evolv_particle_gather/sketch.js
@@ -1,9 +1,11 @@
 let start_pos;
 let stop_pos;
-const r = 20;
+const r = 100;
 const num_points = 100;
-const population_size = 1;
+const population_size = 20;
 let population = [];
+let finished_population = [];
+let population_count = 0;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
@@ -26,5 +28,46 @@ function draw() {
   ellipse(stop_pos.x, stop_pos.y, r);
   pop();
 
-  population[0].show();
+  for (let i = population.length-1; i >= 0; i--) {
+    population[i].update();  
+    if(population[i].life <= 0){
+      population[i].fitness /= (population[i].elements.length * 0.001);
+      finished_population.push(population[i]);
+      population.splice(i, 1);
+    }
+  }
+
+  if(population.length == 0){
+    create_new_population();
+  }
+  
+  if(population_count % 10 === 0){
+    population[0].show();
+  }
+}
+
+function create_new_population(){
+  finished_population.sort(compare);
+  for (let i = 0; i < finished_population.length/5; i++) {
+    for (let j = 0; j < 5; j++) {
+      let new_genome = finished_population[i].copy();
+      for (let n = 0; n < random(1, 5); n++) {
+        new_genome.mutate();
+      }
+      population.push(new_genome);
+    }
+  }
+  finished_population = [];
+  population_count++;
+  print(population_count);
+}
+
+function compare(a,b) {
+  if (a.fitness > b.fitness){
+    return -1;
+  }
+  if (a.fitness < b.fitness){
+    return 1;
+  }
+  return 0;
 }
